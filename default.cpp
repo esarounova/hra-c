19c5f9cc278bae5706e1fdb7e1e9bf8f70667c65
esarounova <eliska.sarounova@email.cz>
2025-04-06 17:51:00 +0200

    Založeni Gitu

--- default.cpp
+++ default.cpp
@@ -2,15 +2,5 @@
 using namespace std;
 
 int main() {
-int matice [2][3];
-int soucet=0;
-
-cout<< "Zadejte 6 hodnot pro matici 2x3:" << endl;
-for (int i=0; i<2; i++){
-        for (int j = 0; j<3; j++){
-                cin>> matice [i][j];
-                soucet+= matice [i][j];
-        }
-}
-cout<< soucet;
+cout<< "Halo halo pls?";
 }
