f1cc374ac82feeb86dc22895688d013200d1ff15
esarounova <eliska.sarounova@email.cz>
2025-04-20 23:51:28 +0200

    Jmeno postavy- overeni vstupu

--- hra.cpp
+++ hra.cpp
@@ -5,43 +5,78 @@ void vyberPostavy () {
 string postava;
 string jmeno;
 int postava1;
+char postavaAN;
 
-
-int stats [5] = {1, 5, 3, 7, 0};
+int stats [5];
 cout<< "Dobry den, vitejte v me uzasne gamesce" << endl;
 cout<< "Jako prvni si musite vybrat classu vaseho charakteru!" << endl;
 cout<< "Mate na vyber z: \n 1. Mag \n 2. Ranger \n 3. Cleric \n 4. Rogue" << endl;
-
 do {
-cout<< "Vyberte si postavu 1-4" << endl;
-cin>>postava1;
+    do {
+    cout<< "Vyberte si postavu 1-4" << endl;
+    cin>>postava1;
 
-}while(postava1 <=0 || postava1 >=5 );{
+    }while(postava1 <=0 || postava1 >=5 );{
 
-switch (postava1){
+    switch (postava1){
 
-case 1:
-postava="Mag";
-break;
+    case 1:
+    postava="Mag";
+    stats [0] = {1,};
+    stats [1] = {1,};
+    stats [2] = {1,};
+    stats [3] = {1,};
+    stats [4] = {0,};
 
-case 2:
-postava="Ranger";
-break;
+    break;
 
-case 3:
-postava ="Cleric";
-break;
+    case 2:
+    postava="Ranger";
+    stats [0] = {1,};
+    stats [1] = {2,};
+    stats [2] = {2,};
+    stats [3] = {2,};
+    stats [4] = {0,};
+    break;
 
-case 4:
-postava= "Rogue";
-break;
+    case 3:
+    postava ="Cleric";
+    stats [0] = {1,};
+    stats [1] = {3,};
+    stats [2] = {3,};
+    stats [3] = {3,};
+    stats [4] = {0,};
+    break;
 
-default:
-cout<< "Spatne zadana hodnota!" << endl;
+    case 4:
+    postava= "Rogue";
+    stats [0] = {1,};
+    stats [1] = {4,};
+    stats [2] = {4,};
+    stats [3] = {4,};
+    stats [4] = {0,};
+    break;
+
+    default:
+    cout<< "Spatne zadana hodnota!" << endl;
+    }
+cout<< "Level:" << stats [0] << "\n Zivoty:" << stats [1] << "\n Utok:" << stats [2] << "\n Mana" << stats [3] << "\n Zlato:" << stats [4] << endl;
 }
+cout<< "Opravdu chcete byt " << postava << "? (A/N)";
+cin>> postavaAN;
+} while (postavaAN == 'N');{
+cout<< "Vybrali jste postavu: " << postava << endl;
+}
+char jmenoAN;
 
-if (postava1 < 5 && postava1 >0)
-cout<< "Vybrali jste postavu: " << postava << "\n Level:"<< stats[0] << " \n Zivoty:" <<stats[1]<< "\n Utok:" << stats[2]<< "\n Mana:" << stats[3] << " \n Zlato:" << stats[4] << endl;
+cout<< "Skvele! Vybrali jste si postavu! \nAle jake bude vase jmeno?"<< endl;
+do{
+cout<< "Zadejte jmeno: ";
+cin>> jmeno;
+cout<< "Jste si jisti jmenem: " << jmeno<< "? (A/N)" << endl;
+cin>> jmenoAN;
+}while (jmenoAN == 'N'); {
+cout<< "Vitej " << jmeno<< endl;
 }
 }
 
