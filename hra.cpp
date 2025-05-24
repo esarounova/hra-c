45a7f09d6056aca0f484cddbd2b820df222a3010
esarounova <eliska.sarounova@email.cz>
2025-05-25 01:13:36 +0200

    Inventar, monstra

--- hra.cpp
+++ hra.cpp
@@ -79,7 +79,12 @@ cout<< "Vitej " << jmeno<< endl;
 }
 }
 
-void inventar (){}
+void inventar (string inve [5]){
+    cout<< "\n Ve vasem inventari se prave nachazi:" <<endl;
+for (int i=0; i< 5; i++){
+    cout<< inve [i] << endl;
+}
+}
 
 void Prespani(int stats [],int rnStats []) {
 if (rnStats [4] < 1) {
@@ -92,10 +97,17 @@ cout<< "Zivoty byly doplneny tvym krasnym slofikem v pokoji hostince, a vsechny 
 cout<< "Nyni musite pokracovat v ceste."<< endl;
 }
 
-int monstrum1 (int rnStats []){
+int monstrum1 (int rnStats [], string inve [], string hlaska, string jmeno){
 int utok;
-cout<< "Po par minutach cesty na tebe z poza kere vyskocil neznamy plaz" << endl;
-cout<< "GRRRR";
+int zlato;
+int n= rand () % 101;
+if (n>50){
+    zlato= rand() % 6;
+}
+cout<< zlato;
+int hp= 2;
+cout<< "Po par minutach cesty na tebe z poza kere vyskocil hrozivý"<< jmeno << endl;
+do{
 do{
 cout<< "Co udelate? \n1. Otevreni inventare \n2. Utok \n3. Pokus o uprch";
 
@@ -103,15 +115,25 @@ cin>> utok;
 } while (utok >3 || utok <1);
         if (utok == 3){
            cout<< "To ted bohuzel nefunguje";
-}
-return 0;
+          } else if (utok == 1){
+    inventar(inve);
+           }
+        else if (utok == 2){
+        cout<< "monstrum rika au au" << endl;
+    }
+} while (hp >=1 || rnStats [1] >=1);
+if (rnStats==0){
+        cout<< "Zemrel jsi v boji" << endl;
+    return 0;
+}else
+cout<< "Vyhral jsi v boji, gratulace";
 }
 
-void Krcma (int rnStats []){
+void Krcma (int rnStats [], string inve [5]){
 int produkt;
 bool koupe;
 string koupeAN;
-int i=1;
+int i=0;
 
 cout<< "Vstoupil jsi do maleho domecku uprostred vesnice, kde te ihned uvita urostly drak"<< endl;
 cout<< "´Zdravim priteli. Co to dnes bude?´"<< endl;
@@ -128,6 +150,7 @@ cout<< "Bez penez to nepujde, kamarade... \n" << endl;
 } else {
 rnStats [4] = rnStats [4] - 7;
 cout<< "Prave jste zakoupili Bilý Monster, gratulujeme!"<< endl;
+inve [i]= "Bilý Monster";
 i++;
 }
     break;
@@ -138,6 +161,7 @@ cout<< "Bez penez to nepujde, kamarade... \n" << endl;
 } else {
 rnStats [4] = rnStats [4] - 3;
 cout<< "Prave jste zakoupili Bagetku z matu, gratulujeme!"<< endl;
+inve [i]= "Bagetku z matu";
 i++;
 }
     break;
@@ -148,6 +172,7 @@ cout<< "Bez penez to nepujde, kamarade... \n" << endl;
 } else {
 rnStats [4] = rnStats [4] - 5;
 cout<< "Prave jste zakoupili Blato v korbelu, gratulujeme!"<< endl;
+inve [i]= "Blato v korbelu";
 i++;
 }
 break;
@@ -161,13 +186,15 @@ cout<< "... Neumis cist? Nic takoveho neprodavame";
 if (produkt!=4) {
 cout<< "Nechces neco jineho? (A/N)" <<endl;
 cin>> koupeAN;
-    if (koupeAN == "A" || koupeAN == "a" || i==5){
+    if (koupeAN == "A" || koupeAN == "a"){
         koupe= 1;
-        if (i==5){
+    }
+        else if (i==5){
             cout<< "Nemate dostatek mista v inventari"<< endl;
             koupe=0;
+        } else if (koupeAN == "N" || koupeAN == "n"){
+        koupe = 0;
         }
-}
 }
 } while (koupe !=0);{
 cout<< "Na videnou, priteli hospodsky rekne behem toho co odchazis z krcmy \n" << endl;
@@ -175,7 +202,7 @@ cout<< "Na videnou, priteli hospodsky rekne behem toho co odchazis z krcmy \n" <
 }
 
 
-void Vesnice (int rnStats[], int stats[], string jmenoV) {
+void Vesnice (int rnStats[], int stats[], string jmenoV, string inve [5]) {
 int KamDal;
 
 cout<< "Vitej ve vesnici " << jmenoV << ", pruzkumniku! \n \n";
@@ -195,7 +222,7 @@ case 1:
 break;
 
 case 2:
-Krcma(rnStats);
+Krcma(rnStats,inve);
 break;
 
 case 3:
@@ -207,13 +234,16 @@ break;
 
 
 int main (){
+string hlaska;
+string jmeno;
+string inve [5];
 int stats [5];
 int rnStats [5];
 string jmenoV;
 
 vyberPostavy(stats, rnStats);
 jmenoV = "Mývalice";
-Vesnice (stats, rnStats, jmenoV);
-monstrum1 (rnStats);
+Vesnice (stats, rnStats, jmenoV, inve);
+monstrum1 (rnStats, inve, hlaska, jmeno);
 return 0;
 }
